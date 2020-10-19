5a133a22eca420dc5ee1553669f454e92f5f07be
sweenutt <sweenutt@192.168.3.19>
2020-10-20 02:00:43 +0700

    Latihan 8 Bilangan Pecahan

---
+++ src/pbo2/if2/pkg10116414/latihan8/pecahan/Pecahan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.if2.pkg10116414.latihan8.pecahan;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * DESKRIPSI    : Program Bilangan Pecahan
+ */
+public class Pecahan {
+    /**
+     * @param args the command line arguments
+     */
+       public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float \t: "+ f);
+        System.out.println("Double \t: "+ d);            
+    }
+}
